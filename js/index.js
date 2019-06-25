f83ce5c3950de8e1ffedaa2b794daf50e685e8c9
gisellehargrove <gisellehargrove@gmail.com>
2019-06-25 18:09:34 -0400

    completed about us page

--- js/index.js
+++ js/index.js
@@ -1,39 +1,42 @@
-// Give the logo image a nice animation on load of the page using GSAP
-TweenMax.from(
-  '.logo-img',
-  2,
-  {
-    rotation: 360,
-    y: -500,
-    ease: Bounce.easeOut
-  }
-);
+$(document).ready(() => {
+  // Give the logo image a nice animation on load of the page using GSAP
+  TweenMax.from(
+    '.logo-img',
+    2,
+    {
+      rotation: 360,
+      y: -500,
+      ease: Bounce.easeOut,
+      delay: 1
+    }
+  );
 
-TweenMax.from(
-  '.page-title',
-  0.5,
-  {
-    opacity: 0,
-    delay: 2
-  }
-)
+  TweenMax.from(
+    '.page-title',
+    0.5,
+    {
+      opacity: 0,
+      delay: 2
+    }
+  )
 
-// Animate the cards into the page
-// Left side info card
-TweenMax.from(
-  '.card-1',
-  0.5,
-  {
-    x: -700,
-    delay: 3
-  }
-)
-// Animate the right side info card
-TweenMax.from(
-  '.card-2',
-  0.5,
-  {
-    x: 700,
-    delay: 4
-  }
-)
+  // Animate the cards into the page
+  // Left side info card
+  TweenMax.from(
+    '.card-1',
+    0.5,
+    {
+      x: -700,
+      delay: 3
+    }
+  )
+  // Animate the right side info card
+  TweenMax.from(
+    '.card-2',
+    0.5,
+    {
+      x: 700,
+      delay: 4
+    }
+  )
+});
