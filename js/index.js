0fc009e290302ee06d83496e688317d6cbc62857
gisellehargrove <gisellehargrove@gmail.com>
2019-06-25 14:43:43 -0400

    adding animations

--- js/index.js
+++ js/index.js
@@ -1,9 +1,39 @@
-const logoImg = document.querySelector('.logo-img');
 // Give the logo image a nice animation on load of the page using GSAP
-TweenMax.to(
+TweenMax.from(
   '.logo-img',
   2,
   {
-    rotation: 360
+    rotation: 360,
+    y: -500,
+    ease: Bounce.easeOut
   }
 );
+
+TweenMax.from(
+  '.page-title',
+  0.5,
+  {
+    opacity: 0,
+    delay: 2
+  }
+)
+
+// Animate the cards into the page
+// Left side info card
+TweenMax.from(
+  '.card-1',
+  0.5,
+  {
+    x: -700,
+    delay: 3
+  }
+)
+// Animate the right side info card
+TweenMax.from(
+  '.card-2',
+  0.5,
+  {
+    x: 700,
+    delay: 5
+  }
+)
