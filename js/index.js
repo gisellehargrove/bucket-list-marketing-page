08b98be9bdf8d4e382b9a27f2430314fb9a9fdb2
gisellehargrove <gisellehargrove@gmail.com>
2019-06-27 12:07:39 -0400

    refactoring less files

--- js/index.js
+++ js/index.js
@@ -2,30 +2,30 @@ $(document).ready(() => {
 
 
   // Not working
-  $('#scroll').click(function(){
-    let top = $(this).offset().top + 75;
-      $('html, body').animate({
-        scrollTop: $(document).height()
-      }, 1000);
-      return false;
-  });
+  // $('#scroll').click(function(){
+  //   let top = $(this).offset().top + 75;
+  //     $('html, body').animate({
+  //       scrollTop: $(document).height()
+  //     }, 1000);
+  //     return false;
+  // });
 
 
 
 
   /***********  ANIMATIONS  ***********/
   // Give the logo image a nice animation on load of the page using GSAP
-  TweenMax.from(
-    '.logo-img',
-    2,
-    {
-      rotation: 360,
-      y: -500,
-      ease: Bounce.easeOut,
-      delay: 1,
-      opacity: 0
-    }
-  );
+  // TweenMax.from(
+  //   '.logo-img',
+  //   2,
+  //   {
+  //     rotation: 360,
+  //     y: -500,
+  //     ease: Bounce.easeOut,
+  //     delay: 1,
+  //     opacity: 0
+  //   }
+  // );
 
   // TweenMax.from(
   //   '.page-title',
@@ -36,31 +36,32 @@ $(document).ready(() => {
   //   }
   // )
 
-  TweenMax.to(
-    '.page-title',
-    1,
-    {
-      opacity: 1,
-      delay: 3
-    }
-  );
+  // TweenMax.to(
+  //   '.page-title',
+  //   1,
+  //   {
+  //     opacity: 1,
+  //     delay: 3
+  //   }
+  // );
 
   const featureCards = $('.mdl-card');
 
   let time = 1;
 
-  featureCards.each((index, card) => {
-
-    TweenMax.from(
-      card,
-      time,
-      {
-        x: -1500,
-        delay: 4
-      }
-    )
-    time += 0.25;
-  });
+  // featureCards.each((index, card) => {
+  //
+  //   TweenMax.from(
+  //     card,
+  //     time,
+  //     {
+  //       x: -1500,
+  //       delay: 4,
+  //       transform: 1.2
+  //     }
+  //   )
+  //   time += 0.25;
+  // });
 
   // Animate the cards into the page
   // Left side info card
